d2cbf25e15299c9fdaea6c1b9f5731d7441481b3
Gourav Verma <gouravv.contact@gmail.com>
2026-01-25 04:48:22 +0000

    fixed server listen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import http from "http";
 import generateRoute from "./api/routes/generate.js";
 import jobRoute from "./api/routes/job.js";
 import "dotenv/config";
 import cors from "cors";
+import { createWSServer } from "./api/ws/wsServer.js";
 
 const app = express();
 app.use(express.json());
@@ -14,7 +16,14 @@ app.use(
   }),
 );
 
+const server = http.createServer(app);
+createWSServer(server);
+
 app.use("/api/v1/generate", generateRoute);
 app.use("/api/v1/job", jobRoute);
 
-app.listen(process.env.PORT);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`HTTP + WS server running on port ${PORT}`);
+});
